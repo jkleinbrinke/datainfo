4dc897c89897c8fafd9373fa8df9c53f82f2e33b
FrostingNL <frostingnl@gmail.com>
2014-04-22 15:49:52 +0200

    ffs

---
+++ session0/src/main/java/nl/utwente/di/Quoter.java
@@ -0,0 +1,15 @@
+package nl.utwente.di;
+
+public class Quoter {
+	public double getBookPrice(String isbnString) {
+		int isbn = Integer.parseInt(isbnString);
+		switch(isbn){
+		case 1: return 10.0;
+		case 2: return 45.0;
+		case 3: return 20.0;
+		case 4: return 35.0;
+		case 5: return 50.0;
+		default: return 0.0;
+		}
+	}
+}
