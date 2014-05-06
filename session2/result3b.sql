4c2873d03895ee4b0cc68e8fe9708c18f992cea0
FrostingNL <frostingnl@gmail.com>
2014-05-06 16:31:50 +0200

    shunting rule

--- session2/result3b.sql
+++ session2/result3b.sql
@@ -1,7 +1,5 @@
-﻿SELECT p.pid FROM Person p
-WHERE EXISTS(
-	SELECT * FROM Acts a, Movie m
+﻿SELECT p.pid FROM Person p, Acts a, Movie m
 	WHERE a.pid = p.pid
 		AND m.mid = a.mid
 		AND m.name = 'Back to the Future'
-	);
+		AND p.pid IS NOT NULL
