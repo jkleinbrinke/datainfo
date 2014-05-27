be3626a93726ef7d23f96acff33f58ddf5111164
FrostingNL <frostingnl@gmail.com>
2014-05-27 15:03:24 +0200

    exercise1f af

---
+++ session5/exercise1f.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION verwijderalles() 
+RETURNS TRIGGER
+AS $$ BEGIN
+	DELETE FROM exemplaar WHERE isbn = OLD.isbn;
+	RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+
+CREATE TRIGGER verwijderexemplaren AFTER DELETE ON boek
+FOR EACH ROW EXECUTE PROCEDURE verwijderalles();
